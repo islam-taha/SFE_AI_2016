1d14d9acecdd6cd5a07badd2e7340be3fefabe48
islam-taha <islamtaha53@gmail.com>
2016-12-23 21:00:37 +0200

    removed rdfs function

--- search.py
+++ search.py
@@ -21,130 +21,109 @@ import util
 
 
 class SearchProblem:
-  """
-  This class outlines the structure of a search problem, but doesn't implement
-  any of the methods (in object-oriented terminology: an abstract class).
-
-  You do not need to change anything in this class, ever.
-  """
-
-  def getStartState(self):
     """
-    Returns the start state for the search problem.
-    """
-    util.raiseNotDefined()
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
 
-  def isGoalState(self, state):
+    You do not need to change anything in this class, ever.
     """
-      state: Search state
 
-    Returns True if and only if the state is a valid goal state.
-    """
-    util.raiseNotDefined()
+    def getStartState(self):
+        """
+        Returns the start state for the search problem.
+        """
+        util.raiseNotDefined()
 
-  def getSuccessors(self, state):
-    """
-      state: Search state
+    def isGoalState(self, state):
+        """
+          state: Search state
 
-    For a given state, this should return a list of triples, (successor,
-    action, stepCost), where 'successor' is a successor to the current
-    state, 'action' is the action required to get there, and 'stepCost' is
-    the incremental cost of expanding to that successor.
-    """
-    util.raiseNotDefined()
+        Returns True if and only if the state is a valid goal state.
+        """
+        util.raiseNotDefined()
 
-  def getCostOfActions(self, actions):
-    """
-     actions: A list of actions to take
+    def getSuccessors(self, state):
+        """
+          state: Search state
 
-    This method returns the total cost of a particular sequence of actions.
-    The sequence must be composed of legal moves.
-    """
-    util.raiseNotDefined()
+        For a given state, this should return a list of triples, (successor,
+        action, stepCost), where 'successor' is a successor to the current
+        state, 'action' is the action required to get there, and 'stepCost' is
+        the incremental cost of expanding to that successor.
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves.
+        """
+        util.raiseNotDefined()
 
 
 def tinyMazeSearch(problem):
-  """
-  Returns a sequence of moves that solves tinyMaze.  For any other maze, the
-  sequence of moves will be incorrect, so only use this for tinyMaze.
-  """
-  from game import Directions
-  s = Directions.SOUTH
-  w = Directions.WEST
-  return [s, s, w, s, w, w, s, w]
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+    sequence of moves will be incorrect, so only use this for tinyMaze.
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return [s, s, w, s, w, w, s, w]
 
 
 def general_search(ds, problem):
-  visited = []
-  ds.push([(problem.getStartState(), "Stop", 0)])
-  while not ds.isEmpty():
-    path = ds.pop()
-    v = path[-1][0]
-    if problem.isGoalState(v):
-      btngana = [u[1] for u in path][1:]
-      return btngana
-    if v not in visited:
-      visited.append(v)
-      for u in problem.getSuccessors(v):
-        if u[0] not in visited:
-          cur_path = path[:]
-          cur_path.append(u)
-          ds.push(cur_path)
-  return []
-
-def rDFS(problem, curr_state, visited, actions):
-  if problem.isGoalState(curr_state):
-    global answer
-    answer = actions[:]
-    raise Exception
-
-  for state, action, cost in problem.getSuccessors(curr_state):
-    if state not in visited:
-      visited.append(state)
-      actions.append(action)
-      rDFS(problem, state, visited, actions)
-      actions.pop()
-
-  return actions
+    visited = []
+    ds.push([(problem.getStartState(), "Stop", 0)])
+    while not ds.isEmpty():
+        path = ds.pop()
+        v = path[-1][0]
+        if problem.isGoalState(v):
+            btngana = [u[1] for u in path][1:]
+            return btngana
+        if v not in visited:
+            visited.append(v)
+            for u in problem.getSuccessors(v):
+                if u[0] not in visited:
+                    cur_path = path[:]
+                    cur_path.append(u)
+                    ds.push(cur_path)
+    return []
 
 
 def depthFirstSearch(problem):
-  try:
-    rDFS(problem, problem.getStartState(), [], [])
-  except Exception as error:
-    print answer
-
-  return answer
-  # return general_search(util.Stack(), problem)
+    return general_search(util.Stack(), problem)
 
 
 def breadthFirstSearch(problem):
-  """Search the shallowest nodes in the search tree first."""
-  return general_search(util.Queue(), problem)
+    """Search the shallowest nodes in the search tree first."""
+    return general_search(util.Queue(), problem)
 
 
 def uniformCostSearch(problem):
-  """Search the node of least total cost first."""
-  return general_search(util.PriorityQueueWithFunction(
-    lambda path:
-    problem.getCostOfActions([v[1] for v in path])), problem)
+    """Search the node of least total cost first."""
+    return general_search(util.PriorityQueueWithFunction(
+        lambda path:
+        problem.getCostOfActions([v[1] for v in path])), problem)
 
 
 def nullHeuristic(state, problem=None):
-  """
-  A heuristic function estimates the cost from the current state to the nearest
-  goal in the provided SearchProblem.  This heuristic is trivial.
-  """
-  return 0
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
 
 
 def aStarSearch(problem, heuristic=nullHeuristic):
-  """Search the node that has the lowest combined cost and heuristic first."""
-  return general_search(util.PriorityQueueWithFunction(
-    lambda path:
-    problem.getCostOfActions([v[1] for v in path]) +
-    heuristic(path[-1][0], problem)
-  ), problem)
+    """Search the node that has the lowest combined cost and heuristic first."""
+    return general_search(util.PriorityQueueWithFunction(
+        lambda path:
+        problem.getCostOfActions([v[1] for v in path]) +
+        heuristic(path[-1][0], problem)
+    ), problem)
 
 
 # Abbreviations
